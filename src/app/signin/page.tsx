88f0556e90ead3230b6162b7c7434620cadb7e7b
ronaudo9 <naoyuki.kojima.0417@gmail.com>
2024-05-15 17:06:50 +0900

    react-hook-formの導入

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,32 @@
+"use client";
 import LockOpenSharpIcon from "@mui/icons-material/LockOpenSharp";
 import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { signInSchema } from "../Utils/validationSchema";
+
+type Login = {
+  email: string;
+  password: string;
+};
 export default function Page() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Login>({
+    mode: "onChange",
+    resolver: zodResolver(signInSchema),
+  });
+  const onSubmit = (data: Login) => {
+    console.log(data);
+  };
+
   return (
-    <div className="mx-auto mt-32 flex w-3/5 flex-col items-center justify-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mx-auto mt-32 flex w-3/5 flex-col items-center justify-center"
+    >
       <div className="mb-3 rounded-full bg-purple-500 p-2">
         <LockOpenSharpIcon sx={{ color: "white" }} />
       </div>
@@ -12,14 +36,20 @@ export default function Page() {
           type="text"
           className="h-12 w-full rounded border p-2"
           placeholder="Email Address*"
+          id="email"
+          {...register("email")}
         />
+        <p className="text-red-400">{errors.email?.message}</p>
       </div>
       <div className="mb-4 w-full md:w-1/2">
         <input
           type="text"
           className="h-12 w-full rounded border p-2"
           placeholder="Password*"
+          id="password"
+          {...register("password")}
         />
+        <p className="text-red-400">{errors.password?.message}</p>
       </div>
       <div className="mb-7 flex  w-full items-center md:w-1/2">
         <input type="checkbox" className="mr-2" />
@@ -48,6 +78,6 @@ export default function Page() {
           </Link>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
